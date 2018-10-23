ba15cb7aec3bf165bc285aeab0896327986ed786
Jose Avalos-Thompson <thompsonjose3@gmail.com>
2018-10-22 19:02:46 -0500

    got it

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         Document lingDoc = Jsoup.connect("http://catalog.illinois.edu/courses-of-instruction/ling/")
                 .userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
@@ -18,13 +18,30 @@ public class Main {
                 .get();
 
 
-        Elements lingCourses = lingDoc.select("courseblock");
+        Elements lingCourses = lingDoc.select("div.courses");
         // course class = courseblock
         String text = lingCourses.text();
-        for ( Element lingCourse: lingCourses) {
-            System.out.println(text);
+
+        for (Element lingCourse : lingCourses) {
+            // the new Element of lingCourse (= 0) in an array of lingCourses (lines))
+            System.out.println(text.replace(" ","\n"));
         }
-//        System.out.println(lingStringCourses);
+
     }
 
+//    public String elementCollector() throws Exception{
+//        Document doc = Jsoup.connect("http://catalog.illinois.edu/courses-of-instruction/ling/")
+//                .userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
+//                .referrer("http://www.google.com")
+//                .get();
+//        // grabbed from URL of website and userAgent proves to the website not spiderbot
+//
+//        Elements titles = doc.select("courseblock");
+//        String text = titles.text();
+//
+////        for (Element title : titles) {
+////            System.out.println(text);
+//
+//        return text;
+//    }
 }
